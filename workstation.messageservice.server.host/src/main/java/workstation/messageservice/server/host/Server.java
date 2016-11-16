f1750621fee2fb45b6b9252b1ee3f882f005f63b
jia19831215@msn.com <jia19831215@msn.com>
2016-11-16 18:44:23 +0800

    111

--- workstation.messageservice.server.host/src/main/java/workstation/messageservice/server/host/Server.java
+++ workstation.messageservice.server.host/src/main/java/workstation/messageservice/server/host/Server.java
@@ -1,10 +1,15 @@
 package workstation.messageservice.server.host;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * Created by Administrator on 2016/11/16.
  */
+
+@ComponentScan
 public class Server {
     public static void main(String[] args) {
-
+        SpringApplication.run(Server.class, args);
     }
 }
