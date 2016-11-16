e10bee62c34d007d217a5e279a3f83305c49e978
jia19831215@msn.com <jia19831215@msn.com>
2016-11-16 15:47:34 +0800

    111

---
+++ workstation.messageservice.server.host/src/main/java/workstation/messageservice/server/host/Server.java
@@ -0,0 +1,10 @@
+package workstation.messageservice.server.host;
+
+/**
+ * Created by Administrator on 2016/11/16.
+ */
+public class Server {
+    public static void main(String[] args) {
+        
+    }
+}
