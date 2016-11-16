f082a747ec00456aadd9d73b1179d6632d12d4c9
jia19831215@msn.com <jia19831215@msn.com>
2016-11-16 18:39:17 +0800

    111

---
+++ workstation.messageservice.services/src/main/java/workstation/messageservice/services/TestService.java
@@ -0,0 +1,7 @@
+package workstation.messageservice.services;
+
+/**
+ * Created by Administrator on 2016/11/16.
+ */
+public interface TestService {
+}
---
+++ workstation.messageservice.services/src/main/java/workstation/messageservice/services/TestServiceImpl.java
@@ -0,0 +1,10 @@
+package workstation.messageservice.services;
+
+/**
+ * Created by Administrator on 2016/11/16.
+ */
+
+
+public class TestServiceImpl implements TestService {
+
+}
