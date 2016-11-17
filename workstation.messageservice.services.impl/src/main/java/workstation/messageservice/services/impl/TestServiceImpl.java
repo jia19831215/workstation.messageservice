bdcc8806c2c9e0418baac23cc571a4dc0287f1e1
jia19831215@msn.com <jia19831215@msn.com>
2016-11-17 09:15:44 +0800

    111

---
+++ workstation.messageservice.services.impl/src/main/java/workstation/messageservice/services/impl/TestServiceImpl.java
@@ -0,0 +1,10 @@
+package workstation.messageservice.services.impl;
+
+import workstation.messageservice.services.TestService;
+
+/**
+ * Created by Administrator on 2016/11/17.
+ */
+public class TestServiceImpl implements TestService {
+
+}
