da7318c309a64321c5e54fd7bb003503977cdb1d
jia19831215@msn.com <jia19831215@msn.com>
2016-11-17 09:37:46 +0800

    111

--- workstation.messageservice.server.host/src/main/java/workstation/messageservice/server/host/Server.java
+++ workstation.messageservice.server.host/src/main/java/workstation/messageservice/server/host/Server.java
@@ -1,13 +1,17 @@
 package workstation.messageservice.server.host;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by Administrator on 2016/11/16.
  */
 
 @ComponentScan
+@EnableAutoConfiguration
+@ImportResource({ "classpath:" })
 public class Server {
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
