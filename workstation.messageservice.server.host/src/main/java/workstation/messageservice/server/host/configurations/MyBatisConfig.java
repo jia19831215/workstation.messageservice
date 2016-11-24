6743ae0b7cc3b7fb8df4f6bd9389214273718fe1
jia19831215@msn.com <jia19831215@msn.com>
2016-11-24 18:11:26 +0800

    111

--- workstation.messageservice.server.host/src/main/java/workstation/messageservice/server/host/configurations/MyBatisConfig.java
+++ workstation.messageservice.server.host/src/main/java/workstation/messageservice/server/host/configurations/MyBatisConfig.java
@@ -1,12 +1,16 @@
 package workstation.messageservice.server.host.configurations;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+import workstation.core.exceptions.SystemException;
 import workstation.spring.util.SpringContextUtil;
 
 import javax.annotation.PostConstruct;
@@ -20,8 +24,22 @@ public class MyBatisConfig implements TransactionManagementConfigurer {
     @Qualifier(DataSourceConfig.MYSQL)
     DataSource dataSource;
 
+    @Bean
     @Override
     public PlatformTransactionManager annotationDrivenTransactionManager() {
         return new DataSourceTransactionManager(this.dataSource);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean() {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+
+        // TODO：SqlSessionFactory 初始化
+
+        try {
+            return bean.getObject();
+        } catch (Exception ce) {
+            throw new SystemException("SqlSessionFactory init", ce);
+        }
     }
 }
